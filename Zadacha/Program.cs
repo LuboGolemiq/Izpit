8d95eefac19b6fd8fb18e98f5a56eec3f84b4504
LuboGolemiq <lubomirpalazov19@gmail.com>
2024-03-08 13:21:39 +0200

    ivpitan

---
+++ Zadacha/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i <=5; i++)
+            {
+                if (n < 255)
+                {
+
+                    Console.WriteLine("incufficient capacity");
+                    int answer = 255 - n;
+                    Console.WriteLine($"{answer}");
+                    sum = 0;
+
+                    sum = n + sum;
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ ZadachaEdno/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ZadachaEdno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int broi = 0;
+            int broqch = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+             for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        broi++;
+                        nums[i] += broqch;
+                        
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",",broqch,broi));
+        }
+    }
+}
